c3c5a64a96a568e4cc2cc58b02ed6e48f2b96ada
itsfaisalhossen <itsfaisalhossen@gmail.com>
2026-01-11 19:34:55 +0600

    Solve the all js problem and added to readme file

---
+++ problem8.js
@@ -0,0 +1,11 @@
+function capitalizeWords(str) {
+  let words = str.split(" ");
+
+  let result = words.map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  return result.join(" ");
+}
+
+console.log(capitalizeWords("hello world")); // Hello World
